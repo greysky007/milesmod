fe061707e2082032c746b41465249456e5257de0
greysky <antonysnork@gmail.com>
2022-06-26 16:55:47 +0300

    reformating code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
-    public int calculate (int cost){
-        int result = cost/20;
+    public int calculate(int cost) {
+        int result = cost / 20;
         return result;
     }
 }
